5d2b52fd65b81198a545b48bdf6504169b14e63a
Tanmay Bishnoi <tbishnoi@torontomu.ca>
2026-05-22 22:14:19 +0530

    update build_dev.sh

--- build_dev.sh
+++ build_dev.sh
@@ -2,29 +2,39 @@ clear
 
 # echo "Sourcing underlay from: $percept_ul"
 
-source $percept_ul 
+# source $percept_ul 
 
-export LDFLAGS='-Wl,--no-as-needed'
+# export LDFLAGS='-Wl,--no-as-needed'
+
+# colcon build \
+# 	--packages-select \
+# 		percept_core \
+# 		experiments \
+# 		ga_circular_fields_planner \
+# 	--symlink-install \
+# 	--cmake-args \
+# 		-DCMAKE_CXX_COMPILER=clang++ \
+# 		-DCMAKE_CUDA_COMPILER=nvcc \
+# 		-DCMAKE_CUDA_HOST_COMPILER=clang++ \
+# 		-DCMAKE_BUILD_TYPE=Debug \
+# 		-DCMAKE_CXX_FLAGS="-fno-omit-frame-pointer -w" \
+# 	--packages-ignore \
+# 		percept_interfaces \
+# 		sackmesser \
+# 		sackmesser_ros2 \
+# 		gafro \
+# 		gafro_ros2 \
+# 		gafro_robot_descriptions
+
 
 colcon build \
-	--packages-select \
-		percept_core \
-		experiments \
-		ga_circular_fields_planner \
 	--symlink-install \
 	--cmake-args \
 		-DCMAKE_CXX_COMPILER=clang++ \
 		-DCMAKE_CUDA_COMPILER=nvcc \
 		-DCMAKE_CUDA_HOST_COMPILER=clang++ \
 		-DCMAKE_BUILD_TYPE=Debug \
-		-DCMAKE_CXX_FLAGS="-fno-omit-frame-pointer -w" \
-	--packages-ignore \
-		percept_interfaces \
-		sackmesser \
-		sackmesser_ros2 \
-		gafro \
-		gafro_ros2 \
-		gafro_robot_descriptions
+		-DCMAKE_CXX_FLAGS="-Wno-error=deprecated-declarations"
 
 
 # export LDFLAGS='-Wl,--no-as-needed'
